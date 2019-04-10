2fd5553214dbca9e8ad1df7af34db478641269c8
chokkint <9547328@qq.com>
2019-04-10 09:38:50 +0800

    before_login add

---
+++ app/src/main/java/com/ivantee/smartsell/LoginBeforeActivity.java
@@ -0,0 +1,14 @@
+package com.ivantee.smartsell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginBeforeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_before);
+    }
+}
